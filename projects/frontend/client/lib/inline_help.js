aab8e44dbc01a909a153e9fa9cc84d1288526b43
rbh <ryan@saexa.com>
2015-04-28 01:57:08 +0200

    put jumbotron button back and added help for exposure gauge

---
+++ projects/frontend/client/lib/inline_help.js
@@ -0,0 +1,13 @@
+// Copyright 2015 Ryan B. Hicks
+
+var helpData = {
+  'exposure-gauge-help': {
+    title: "Exposure help document title ",
+    message: "Exposure help document message", //supports Markdown
+    url: "/exposure_gauge_help.html",
+    options: {
+      placement: 'right'
+    }
+  }
+}
+InlineHelp.initHelp(helpData); 
